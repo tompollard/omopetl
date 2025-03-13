e707c761cf4df3aa827c517a53a046ce7cbdcf26
Tom Pollard <tpollard@mit.edu>
2025-03-13 11:18:06 -0400

    rename 'columns' to 'sequence'

--- omopetl/pipeline.py
+++ omopetl/pipeline.py
@@ -90,7 +90,7 @@ def run_etl(project_path, dry=False, casual=False):
         mapping_config = mappings[mapping_name]
         source_table = mapping_config['source_table']
         target_table = mapping_config['target_table']
-        transformations = mapping_config['columns']
+        transformations = mapping_config['sequence']
 
         print(f"Mapping {source_table} -> {target_table}")
 
